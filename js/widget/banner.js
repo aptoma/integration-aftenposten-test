41af4ccc72678e120f0d08c43f9df0f05555bd7b
Øystein J. Songstad <oystein@aptoma.com>
2014-06-02 15:06:58 +0200

    Add second banner script for tablet ads

--- js/widget/banner.js
+++ js/widget/banner.js
@@ -9,7 +9,12 @@ define('js/widgets/banner', ['alf'], function(Alf){
             var container = this.container;
             container.$el.empty();
 
-            var url = "http://adserver.adtech.de/addyn|3.0|995.1|5024934|0|1744|ADTECH;cookie=info;alias=iPad_app_AP_Toppbanner_980x150;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=" + new Date().getTime();
+            var bannerArr = [
+                'http://adserver.adtech.de/addyn|3.0|995.1|5024934|0|1744|ADTECH;cookie=info;alias=iPad_app_AP_Toppbanner_980x150;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=',
+                'http://adserver.adtech.de/addyn|3.0|995.1|5024933|0|1744|ADTECH;cookie=info;alias=iPad_app_AP_Midtbanner1_980x150;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc='
+            ];
+            var randomBanner = Math.floor(Math.random()*bannerArr.length);
+            var url = bannerArr[randomBanner] + new Date().getTime();
 
             var iframe = document.createElement('iframe');
             iframe.className = 'ad-iframe';

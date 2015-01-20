e42b40efef6308ed35d11154a1176eb774c61136
Miroslaw Kucharzyk <miroslaw.kucharzyk@schibsted.pl>
2015-01-20 13:21:48 +0100

    banners for ipad version

--- js/widget/banner.js
+++ js/widget/banner.js
@@ -9,14 +9,50 @@ define('js/widgets/banner', ['alf'], function(Alf){
 
         run: function(done){
             var container = this.container;
+            var sectionName = container.$el.find(":first-child").attr("data-section");
+
             container.$el.empty();
 
-            var bannerArr = [
-                'http://adserver.adtech.de/addyn|3.0|995.1|5024934|0|1744|ADTECH;cookie=info;alias=iPad_app_AP_Toppbanner_980x150;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=',
-                'http://adserver.adtech.de/addyn|3.0|995.1|5024933|0|1744|ADTECH;cookie=info;alias=iPad_app_AP_Midtbanner1_980x150;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc='
-            ];
-            var randomBanner = Math.floor(Math.random()*bannerArr.length);
-            var url = bannerArr[randomBanner] + new Date().getTime();
+
+            var banners = {
+                "nyheter": [
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509585|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Nyheter:TopBoard;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=',
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509596|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Nyheter:SuperBoard1;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc='
+                ],
+                "okonomi": [
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509586|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Okonomi:SuperBoard1;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=',
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509598|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Okonomi:TopBoard;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc='
+                ],
+                "osloby": [
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509587|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Osloby:TopBoard;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=',
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509588|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Osloby:SuperBoard1;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc='
+                ],
+                "reise": [
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509589|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Reise:SuperBoard1;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=',
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509592|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Reise:TopBoard;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc='
+                ],
+                "bil": [
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509591|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Bil:SuperBoard1;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=',
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509593|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Bil:TopBoard;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc='
+                ],
+                "sport": [
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509594|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Sport:TopBoard;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=',
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509597|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Sport:SuperBoard1;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc='
+                ],
+                "kultur": [
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509595|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Kultur:TopBoard;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=',
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509601|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Kultur:SuperBoard1;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc='
+                ],
+                "other": [
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509590|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Other:TopBoard;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc=',
+                    'http://adserver.adtech.de/addyn|3.0|1582.1|5509600|0|1744|ADTECH;cookie=info;alias=SB_AftenpostenPluss_Tablet:Other:SuperBoard1;loc=100;target=_blank;key=key1+key2+key3+key4;grp=[group];misc='
+                ]
+            }
+
+            var sectionBanners = banners[sectionName] || banners['other'];
+
+            var randomBanner = Math.floor(Math.random()*sectionBanners.length);
+            var url = sectionBanners[randomBanner] + new Date().getTime();
 
             var iframe = document.createElement('iframe');
             iframe.className = 'ad-iframe';

bf539c2c5013bf3e5ddc04c17e1ff07caa99d696
Peter Rudolfsen <peter@silencethefoe.com>
2015-08-27 10:35:39 +0200

    Fix issue with disqus articles

--- js/widget/disqus.js
+++ js/widget/disqus.js
@@ -7,7 +7,7 @@ module.exports = {
     selector: '.lp-widget-disqus',
     //selector: 'div',
 
-    run: function(done){
+    run: function (done) {
 
         var $this = this.$el;
         var shortname = $this.attr('shortname');
@@ -17,24 +17,24 @@ module.exports = {
         var treshhold = 100;
         var skip = false;
 
-        function openComments(){
-            if(skip) {
+        function openComments() {
+            if (skip) {
                 console.log('block it');
                 return;
             }
 
             //console.log('Comments for: ' + shortname + ' ' + query);
-            var url = 'http://lisa.aftenposten.no/aftenposten_pluss/stage/integration/disqus.html?shortname='+shortname+'&identifier='+query;
+            var url = 'http://lisa.aftenposten.no/aftenposten_pluss/stage/integration/disqus.html?shortname=' + shortname + '&identifier=' + query;
             window.location.href = url;
 
             skip = true;
-            setTimeout(function(){
+            setTimeout(function() {
                 skip = false;
             }, treshhold);
 
         }
 
-        $this.tap(openComments);
+        $this.on('tap', openComments);
 
         done();
     }
